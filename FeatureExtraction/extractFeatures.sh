cfb04be868cf91d1d13fde7c7c98388d23ff0b1b
doltonfernandes <dolton.fernandes@gmail.com>
2020-11-09 23:17:21 +0530

    Finished feature extraction

--- FeatureExtraction/extractFeatures.sh
+++ FeatureExtraction/extractFeatures.sh
@@ -6,3 +6,7 @@ mkdir features
 praat main.psc
 # Energy features using Python
 python3 energy_extraction.py
+# Speech Rate using Python
+python3 SR_extraction.py
+# Concatenate different features
+python3 concatFeatures.py
